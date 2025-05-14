ad8709c2dcf0061654258c2613c85972027f7fbe
Nikhil Ambure <nikhilambure25@gmail.com>
2025-05-14 22:31:25 +0530

    Setup

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
